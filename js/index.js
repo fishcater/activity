32f4c0400970a1438ba7459e01edbabe8924adcc
mooncat chen <mooncat@localhost.localdomain>
2016-03-08 19:49:47 -0800

    添加图片

--- js/index.js
+++ js/index.js
@@ -102,7 +102,7 @@ function initChoosen(){
 				html+='</dl>';
 			}
 */
-var html = "<img src='/img/code.jpg' />;
+var html = "<img src='/img/code.jpg' />";
 			$('.ppLists .lists ul').html(html)
 		},
 		printme:function(){
